0167a0e52874bebf87f3670e86c45e02e231b7d2
Natalia <semnatalis52@gmail.com>
2023-09-28 20:32:12 +0300

    corrected Homework_07

--- db_scripts/seed.py
+++ db_scripts/seed.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from faker import Faker
-from models import Student, Group, Teacher, Subject, Grade
+from db_scripts.models import Student, Group, Teacher, Subject, Grade
 import random
 from datetime import date, timedelta
 
